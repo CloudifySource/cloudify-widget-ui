c0ec70188bcced2756ab0f39034375724160b961
sefi <sefi@gigaspaces.com>
2014-11-03 09:51:47 +0200

    Small tweak to cloud node mappings section of the combined view.

--- test/system/sanity/index.js
+++ test/system/sanity/index.js
@@ -9,31 +9,17 @@ var assert = require('assert');
 var common = require('../common.js');
 var utils = require('../utils.js');
 var webDriver = require('selenium-webdriver');
-//var driver = common.getChromeDriver();
 
-//var waitForValidLink = function(element, callback) {
-//    logger.info('Waiting for link to be valid');
-//    driver.wait(function () {
-//        return element.getAttribute('href').then(function (value) {
-//            return value !== '#';
-//        });
-//    }, 15000, 'Waited too long').then(function () {
-//        logger.debug('Done!');
-//        callback();
-//    });
-//
-//};
+//sanity test
+var sanityDriver = common.getChromeDriver();
+sanityDriver.get('http://www.google.com');
+var searchBox = sanityDriver.findElement(webDriver.By.name('q'));
+searchBox.sendKeys('webDriver');
+searchBox.getAttribute('value').then(function(value) {
+    assert.equal(value, 'webDriver');
+});
 
-// sanity test
-
-//    driver.get('http://www.google.com');
-//    var searchBox = driver.findElement(webDriver.By.name('q'));
-//    searchBox.sendKeys('webDriver');
-//    searchBox.getAttribute('value').then(function(value) {
-//        assert.equal(value, 'webDriver');
-//    });
-//
-//    driver.quit();
+sanityDriver.quit();
 
 
 //xap demo test
@@ -45,7 +31,6 @@ utils.waitForElementEnabledById(driver, 'launch', function (launchBtn) {
     launchBtn.click();
 
     utils.waitForElementEnabledById(driver, 'use', function (useLink) {
-//        var useLink = driver.findElement(webDriver.By.id('use'));
         useLink.getAttribute('href').then(function (href) {
             logger.info('href = ', href);
             assert(href.indexOf('#') === -1);
