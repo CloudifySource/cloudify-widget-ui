0093dae5f6b0a05dc8a2201249edf2ccb5970b21
LironHazan <liron@gigaspaces.com>
2014-11-26 16:04:06 +0200

    updated the karma.conf file with coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -520,7 +520,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', [
         'jshint',
-//        'test',
+        'test',
         'build'
     ]);
 
--- karma.conf.js
+++ karma.conf.js
@@ -1,53 +1,58 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function (config) {
+    var configuration = {
 
+        basePath: '',
+        frameworks: ['jasmine'],
 // list of files / patterns to load in the browser
-console.log(JASMINE, JASMINE_ADAPTER);
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-    'app/bower_components/jquery/jquery.js',
-    'app/bower_components/angular/angular.js',
-    'app/bower_components/angular-cookies/angular-cookies.js',
-    'app/bower_components/angular-route/angular-route.js',
-    'app/bower_components/angular-sanitize/angular-sanitize.js',
-    'app/bower_components/ngstorage/ngStorage.js',
-    'app/bower_components/angular-resource/angular-resource.js',
-    'app/bower_components/angular-mocks/angular-mocks.js',
-    'app/bower_components/angular-mocks/angular-mocks.js',
-    'app/bower_components/gs-ui-infra/app/scripts/app.js',
-    'app/bower_components/gs-ui-infra/app/scripts/filters/i18n.js',
-    'app/bower_components/gs-ui-infra/app/scripts/services/i18next.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+        //   console.log(JASMINE, JASMINE_ADAPTER);
+        files: [
+            'app/bower_components/jquery/jquery.js',
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.js',
+            'app/bower_components/ngstorage/ngStorage.js',
+            'app/bower_components/angular-resource/angular-resource.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/gs-ui-infra/app/scripts/app.js',
+            'app/bower_components/gs-ui-infra/app/scripts/filters/i18n.js',
+            'app/bower_components/gs-ui-infra/app/scripts/services/i18next.js',
+            'app/scripts/*.js',
+            'app/scripts/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
 // list of files to exclude
-exclude = [];
+        exclude: [],
+
+
+        preprocessors: {
+            'app/scripts/**/*.js': ['coverage']
+        },
 
 // test results reporter to use
 // possible values: dots || progress || growl
-reporters = ['progress'];
+        reporters: ['progress', 'coverage'],
 
 // web server port
-port = 8080;
+        port: 8080,
 
 // cli runner port
-runnerPort = 9100;
+        runnerPort: 9100,
 
 // enable / disable colors in the output (reporters and logs)
-colors = true;
+        colors: true,
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        logLevel: config.LOG_INFO,
 
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        autoWatch: false,
 
 // Start these browsers, currently available:
 // - Chrome
@@ -57,11 +62,40 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+        browsers: ['Chrome'],
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 30000;
+        captureTimeout: 30000,
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+        singleRun: false,
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/',
+            subdir: function (browser) {
+                var result = browser.toLowerCase().split(/[ /-]/)[0];
+                console.log('this is browser', result);
+                return result;
+            }
+        },
+
+        plugins: [
+            'karma-jasmine',
+            'karma-coverage',
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-coverage',
+            'karma-spec-reporter',
+            'karma-junit-reporter'
+        ]
+
+    };
+
+    config.set(configuration);
+
+};
+
+
