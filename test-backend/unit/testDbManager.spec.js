70301a0f847c08085ac6807507abb6f41df842e4
LironHazan <liron@gigaspaces.com>
2014-11-30 11:48:34 +0200

    add fixmyjs

--- test-backend/unit/testDbManager.spec.js
+++ test-backend/unit/testDbManager.spec.js
@@ -1,22 +1,14 @@
+'use strict';
 /**
  * Created by liron on 11/30/14.
  */
-
 var logger = require('log4js').getLogger('testDbManager.spec');
-
 describe('Backend: managers', function () {
-
     var dbManager = require('../../backend/managers/DbManager');
-
-
     it('test dbManager', function () {
-
-
         var collectionName = null;
-        var connection = dbManager.connect(collectionName, function(){});
-
+        var connection = dbManager.connect(collectionName, function () {
+        });
         logger.info('connection = ' + connection);
     });
-
-
 });
--- test-backend/unit/testtry.spec.js
+++ test-backend/unit/testtry.spec.js
@@ -1,33 +1,20 @@
 /**
  * Created by liron on 11/26/14.
  */
-
 'use strict';
-
 // example with spies
-
-var Person = function() {};
-
-Person.prototype.helloSomeone = function(toGreet) {
-    return this.sayHello() + " " + toGreet;
+var Person = function () {
 };
-
-Person.prototype.sayHello = function() {
-    return "Hello";
+Person.prototype.helloSomeone = function (toGreet) {
+    return this.sayHello() + ' ' + toGreet;
 };
-
-
+Person.prototype.sayHello = function () {
+    return 'Hello';
+};
 describe('Backend: walkFolder', function () {
-
     it('has a browseBlueprint', function () {
-        var controller = require('../../backend/controllers/AdminUsersController');
-
         var fakePerson = new Person();
-        spyOn(fakePerson, "sayHello").andReturn("Hello Hello");
-
-        expect( fakePerson.sayHello()).toBe("Hello Hello")
-       // expect(2+2).toBe(4);
+        spyOn(fakePerson, 'sayHello').andReturn('Hello Hello');
+        expect(fakePerson.sayHello()).toBe('Hello Hello');    // expect(2+2).toBe(4);
     });
-
-
 });
