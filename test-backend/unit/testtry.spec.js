6a8f271ec790e9fba46855f0d5137e03e0206ae7
LironHazan <liron@gigaspaces.com>
2014-11-30 10:02:56 +0200

    added a new test for cloudifyCliService

--- test-backend/unit/testtry.spec.js
+++ test-backend/unit/testtry.spec.js
@@ -4,11 +4,29 @@
 
 'use strict';
 
+// example with spies
+
+var Person = function() {};
+
+Person.prototype.helloSomeone = function(toGreet) {
+    return this.sayHello() + " " + toGreet;
+};
+
+Person.prototype.sayHello = function() {
+    return "Hello";
+};
+
+
 describe('Backend: walkFolder', function () {
 
     it('has a browseBlueprint', function () {
         var controller = require('../../backend/controllers/AdminUsersController');
-        expect(2+2).toBe(4);
+
+        var fakePerson = new Person();
+        spyOn(fakePerson, "sayHello").andReturn("Hello Hello");
+
+        expect( fakePerson.sayHello()).toBe("Hello Hello")
+       // expect(2+2).toBe(4);
     });
 
 
