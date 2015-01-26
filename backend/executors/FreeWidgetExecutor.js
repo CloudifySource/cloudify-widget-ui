8cbd2392e72d979bef55e7d084dc18055cc85b21
sefi <sefi@gigaspaces.com>
2015-01-26 15:32:06 +0200

    refactor(WidgetManager): play/play solo
    introduce the TasksDirectory object

--- backend/executors/FreeWidgetExecutor.js
+++ backend/executors/FreeWidgetExecutor.js
@@ -5,8 +5,8 @@
 
 var util = require('util');
 var AbstractWidgetExecutor = require('./AbstractWidgetExecutor');
+var tasksDirectory = require('./TasksDirectory');
 var logger = require('log4js').getLogger('FreeWidgetExecutor');
-var managers = require('../managers');
 
 function FreeWidgetExecutor(executionModel) {
     logger.info('ctor');
@@ -15,42 +15,15 @@ function FreeWidgetExecutor(executionModel) {
 
 util.inherits(FreeWidgetExecutor, AbstractWidgetExecutor);
 
-//--------------------- TASKS -----------------------
-FreeWidgetExecutor.prototype.getPoolKey = function (executionModel, callback) {
-    logger.info('getting pool key');
-
-    managers.db.connect('users', function (db, collection) {
-        collection.findOne({ '_id': executionModel.getWidget().userId }, function (err, result) {
-            if (err) {
-                logger.error('unable to find user from widget', err);
-                callback(err, executionModel);
-                return;
-            }
-
-            if (!result) {
-                logger.error('result is null for widget');
-                callback(new Error('could not find user for widget'), executionModel);
-                return;
-            }
-
-            logger.info('found poolKey', result.poolKey);
-            executionModel.setPoolKey(result.poolKey);
-            callback(null, executionModel);
-        });
-    });
-};
-
-//--------------------- TASKS END -----------------------
-
 //-----------  Overrides  ----------------------
 FreeWidgetExecutor.prototype.executionType = 'Free';
 
 AbstractWidgetExecutor.prototype.getExecutionTasks = function () {
     return [
-        this.getWidget,
-        this.getPoolKey,
-        this.saveExecutionModel,
-        this.updateExecutionModelAddPaths
+        tasksDirectory.getWidget,
+        tasksDirectory.getPoolKey,
+        tasksDirectory.saveExecutionModel,
+        tasksDirectory.updateExecutionModelAddPaths
     ];
 };
 
--- backend/executors/TasksDirectory.js
+++ backend/executors/TasksDirectory.js
@@ -9,6 +9,8 @@ var path = require('path');
 var _ = require('lodash');
 var conf = require('../Conf');
 
+//---------------- COMMON TASKS ------------------------
+
 exports.getWidget = function (executionModel, callback) {
     logger.info('getting widget id ' + executionModel.getWidgetId());
 
@@ -34,3 +36,124 @@ exports.getWidget = function (executionModel, callback) {
         });
     });
 };
+
+exports.saveExecutionModel = function (executionModel, callback) {
+    logger.info('saving execution to mongo');
+
+    managers.db.connect('widgetExecutions', function (db, collection, done) {
+        // instantiate the execution model with the widget data, and remove the _id - we want mongodb to generate a unique id
+        var storedExecutionModel = {};
+        storedExecutionModel.widget = executionModel.getWidget();
+        storedExecutionModel.loginDetailsId = executionModel.getLoginDetailsId();
+        storedExecutionModel.state = 'RUNNING';
+
+        collection.insert(storedExecutionModel, function (err, docsInserted) {
+            if (err) {
+                logger.error('failed to store widget execution model to DB', err);
+                callback(err, executionModel);
+                done();
+                return;
+            }
+
+            if (!docsInserted) {
+                logger.error('no widget execution docs inserted to database');
+                callback(new Error('no widget execution docs inserted to database'), executionModel);
+                done();
+                return;
+            }
+
+            executionModel.setExecutionObjectId(docsInserted[0]._id);
+            callback(null, executionModel);
+            done();
+        });
+    });
+};
+
+exports.updateExecutionModel = function (data, executionModel, callback) {
+    managers.db.connect('widgetExecutions', function (db, collection, done) {
+        collection.findOne(
+            { _id: executionModel.getExecutionObjectId() },
+            function (err, result) {
+                if (err) {
+                    logger.error('failed to retrieve execution model before update', err);
+                    callback(err, executionModel);
+                    done();
+                    return;
+                }
+
+                result = _.merge(result, data);
+
+                collection.update(
+                    { _id: executionModel.getExecutionObjectId() },
+                    result,
+                    function (err, nUpdated) {
+                        if (err) {
+                            logger.error('failed updating widget execution model', err);
+                            callback(err, executionModel);
+                            done();
+                            return;
+                        }
+
+                        if (!nUpdated) {
+                            logger.error('no widget execution docs updated in the database');
+                            callback(new Error('no widget execution docs updated in the database'), executionModel);
+                            done();
+                            return;
+                        }
+
+                        callback(null, executionModel);
+                        done();
+                    });
+            });
+    });
+};
+
+exports.updateExecutionModelAddPaths = function (executionModel, callback) {
+    logger.info('updating execution model add paths');
+
+    executionModel.setExecutionId(executionModel.getExecutionObjectId().toHexString());
+    executionModel.setDownloadsPath(path.join(conf.downloadsDir, executionModel.getExecutionId()));
+    executionModel.setLogsPath(path.join(conf.logsDir, executionModel.getExecutionId()));
+
+    this.updateExecutionModel({
+        downloadsPath: executionModel.getDownloadsPath(),
+        logsPath: executionModel.getLogsPath()
+    }, executionModel, callback);
+};
+
+//---------------- COMMON TASKS END ------------------------
+
+//---------------- FREE TASKS ------------------------
+
+exports.getPoolKey = function (executionModel, callback) {
+    logger.info('getting pool key');
+
+    managers.db.connect('users', function (db, collection) {
+        collection.findOne({ '_id': executionModel.getWidget().userId }, function (err, result) {
+            if (err) {
+                logger.error('unable to find user from widget', err);
+                callback(err, executionModel);
+                return;
+            }
+
+            if (!result) {
+                logger.error('result is null for widget');
+                callback(new Error('could not find user for widget'), executionModel);
+                return;
+            }
+
+            logger.info('found poolKey', result.poolKey);
+            executionModel.setPoolKey(result.poolKey);
+            callback(null, executionModel);
+        });
+    });
+};
+
+//---------------- FREE TASKS END ------------------------
+
+
+//---------------- SOLO TASKS ------------------------
+
+
+//---------------- SOLO TASKS END ------------------------
+
