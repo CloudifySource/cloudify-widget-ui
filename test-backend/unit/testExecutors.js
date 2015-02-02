ea2a7677596f28c94c85358fa77a6df62a033ac4
sefi <sefi@gigaspaces.com>
2015-02-02 16:08:32 +0200

    feat(softlayerSolo): play solo softlayer
    base solo softlayer flow

--- test-backend/unit/testExecutors.js
+++ test-backend/unit/testExecutors.js
@@ -3,9 +3,9 @@
  */
 'use strict';
 
-var executors = require('../../../backend/executors');
+var executors = require('../../backend/executors/index');
 var logger = require('log4js').getLogger('testExecutors.spec');
-var testConf = require('../conf/dev/me-test-conf.json');
+var testConf = require('./conf/dev/me-test-conf.json');
 
 // Free test
 //var executionModel = new executors.ExecutionModel('53d651d37818c889b6619020', function () {
@@ -44,26 +44,26 @@ var testConf = require('../conf/dev/me-test-conf.json');
 //executor.play(executionModel);
 
 // Solo softlayer test
-//var executionDetails = {
-//    isSoloMode: true,
-//    providerUrl: 'https://www.dropbox.com/s/f44ngahig9k077w/ec2.tar.gz?dl=1',
-//    appName: 'default',
-//    serviceName: 'mongod',
-//    providerRootPath: 'ec2',
-//    providerName: 'softlayer',
-//    privateAmiId: 'ami-3cb92054',
-//    privateAmiRegion: 'us-east-1',
-//    privateImageId: 'ami-7aa83112',
-//    privateImageRegion: 'us-east-1',
-//    privateImages: [],
-//    softlayer: testConf.softlayer
-//};
-//
-//var executionModel = new executors.SoloExecutionModel('53d651d37818c889b6619020', function () {
-//    logger.info('executionCallback');
-//});
-//executionModel.setExecutionDetails(executionDetails);
-//
-//var executor = new executors.SoloSoftlayerWidgetExecutor();
-//executor.play(executionModel);
+var executionDetails = {
+    isSoloMode: true,
+    providerUrl: 'https://www.dropbox.com/s/f44ngahig9k077w/ec2.tar.gz?dl=1',
+    appName: 'default',
+    serviceName: 'mongod',
+    providerRootPath: 'ec2',
+    providerName: 'softlayer',
+    privateAmiId: 'ami-3cb92054',
+    privateAmiRegion: 'us-east-1',
+    privateImageId: 'ami-7aa83112',
+    privateImageRegion: 'us-east-1',
+    privateImages: [],
+    softlayer: testConf.softlayer
+};
+
+var executionModel = new executors.SoloExecutionModel('53d651d37818c889b6619020', function () {
+    logger.info('executionCallback');
+});
+executionModel.setExecutionDetails(executionDetails);
+
+var executor = new executors.SoloSoftlayerWidgetExecutor();
+executor.play(executionModel);
 
