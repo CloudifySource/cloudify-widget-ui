f419f6c434ed1de77f495a5659b71af61ad37e06
LironHazan <liron@gigaspaces.com>
2015-04-30 14:23:14 +0300

    feat(post-bootstrap script) added script for post bootstrap as appears on pool mgmt

--- build/pool-mgmt/post-manager-bootstrap-for-get-cloudify.sh
+++ build/pool-mgmt/post-manager-bootstrap-for-get-cloudify.sh
@@ -1,12 +1,16 @@
 set -e
 
-# bootstrap_script located in bucket=cloudify-widget-bootstrap of ec2automations !!
-#
-sudo wget "https://s3.amazonaws.com/cloudify-widget-bootstrap/ops+provision+script/bootstrap_script.sh"
+# old script bootstrap_script located in bucket=cloudify-widget-bootstrap of ec2automations !!
+#"https://s3.amazonaws.com/cloudify-widget-bootstrap/ops+provision+script/bootstrap_script.sh"
+# new script doesn't work.
+# sudo wget https://github.com/cloudify-cosmo/cloudify-packager/blob/495db9092bbea660474a17dc57eeb8058e0f2aae/packer/vagrant/provision.sh
+
+
 
 source bootstrap_script.sh
 
-wget "https://github.com/cloudify-cosmo/cloudify-nodecellar-example/archive/cloudify-widget-3.1.zip" -O blueprint.zip
+# cloudify 3.1 : cloudify-widget-3.1 changed to cloudify 3.2 :  cloudify-widget-3.2
+wget "https://github.com/cloudify-cosmo/cloudify-nodecellar-example/archive/cloudify-widget-3.2.zip" -O blueprint.zip
 
 echo "#################### update and clean source ######################"
 sudo apt-get update
@@ -23,7 +27,7 @@ sudo apt-get -y install zip unzip
 echo "#################### running unzip ######################"
 unzip -n blueprint.zip
 
-cd cloudify-nodecellar-example-cloudify-widget-3.1
+cd cloudify-nodecellar-example-cloudify-widget-3.2
 echo "#################### uploading nodecellar blueprint ######################"
 cfy blueprints upload -b nodecellar1 -p singlehost-blueprint.yaml -v
 
